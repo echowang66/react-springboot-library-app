dbcc60ff51b23d3d2fae08dd2dbb781dfa94e594
echowang66 <echowangcn@outlook.com>
2023-09-30 21:39:05 +0100

    update search books function

--- 03-frontend/react-library/src/App.tsx
+++ 03-frontend/react-library/src/App.tsx
@@ -4,14 +4,14 @@ import "./App.css";
 import { Navbar } from "./layouts/NavbarAndFooter/Navbar";
 import { Footer } from "./layouts/NavbarAndFooter/Footer";
 import { HomePage } from "./layouts/HomePage/HomePage";
-import { SearchBookPage } from "./layouts/SearchBookPage/SearchBookPage";
+import { SearchBooksPage } from "./layouts/SearchBookPage/SearchBooksPage";
 
 export const App = () => {
   return (
     <div>
       <Navbar />
       {/* <HomePage/> */}
-      <SearchBookPage />
+      <SearchBooksPage />
       <Footer />
     </div>
   );
