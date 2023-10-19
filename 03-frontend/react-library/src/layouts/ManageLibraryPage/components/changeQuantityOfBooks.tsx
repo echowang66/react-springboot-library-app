081916ca5fff27a9d27e1eddb54ce91421be008a
echowang66 <echowangcn@outlook.com>
2023-10-19 23:24:30 +0100

    finish the admin functions

--- 03-frontend/react-library/src/layouts/ManageLibraryPage/components/changeQuantityOfBooks.tsx
+++ 03-frontend/react-library/src/layouts/ManageLibraryPage/components/changeQuantityOfBooks.tsx
@@ -13,6 +13,9 @@ export const ChangeQuantityOfBooks = () => {
   const [booksPerPage] = useState(5);
   const [totalAmountOfBooks, setTotalAmountOfBooks] = useState(0);
   const [totalPages, setTotalPages] = useState(0);
+  // book delete
+
+  const[bookDelete, setBookDelete] = useState(false);
 
   useEffect(() => {
     const fetchBooks = async () => {
@@ -55,7 +58,7 @@ export const ChangeQuantityOfBooks = () => {
       setIsLoading(false);
       setHttpError(error.message);
     });
-  }, [currentPage]);
+  }, [currentPage,bookDelete]);
 
   const indexOfLastBook: number = currentPage * booksPerPage;
   const indexOfFirstBook: number = indexOfLastBook - booksPerPage;
@@ -65,6 +68,8 @@ export const ChangeQuantityOfBooks = () => {
       : totalAmountOfBooks;
 
   const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
+
+  const deleteBook = () => setBookDelete(!bookDelete);
 
   if (isLoading) {
     return <SpinnerLoading />;
@@ -89,7 +94,7 @@ export const ChangeQuantityOfBooks = () => {
             {indexOfFirstBook + 1} to {lastItem} of {totalAmountOfBooks} items:
           </p>
           {books.map((book) => (
-           <ChangeQuantityOfBook book = {book} key = {book.id}/>
+           <ChangeQuantityOfBook book = {book} key = {book.id} deleteBook = {deleteBook}/>
           ))}
         </>
       ) : (
